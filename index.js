cf45cf4228e324d46b9368192efeb2005986206c
OV <o.varlamov@globexit.ru>
2026-03-11 16:46:31 +0300

    Задача1: Реализовать функционал по изменению названия заметки по ID

--- index.js
+++ index.js
@@ -1,55 +1,103 @@
-const express = require('express')
-const chalk = require('chalk')
-const path = require('path')
-const { addNote, getNotes, removeNote, updateNote } = require('./notes.controller')
+const yargs = require("yargs");
 
-const port = 3000
-const app = express()
+yargs.command({
+  command: "add",
+  describe: "add new note",
+  handler() {
+    console.log("add command");
+  },
+});
 
-app.set('view engine', 'ejs')
-app.set('views', 'pages')
+yargs.command({
+  command: "list",
+  describe: "print all nodes",
+  handler() {
+    console.log("list command1");
+  },
+});
 
-app.use(express.static(path.resolve(__dirname, 'public')))
-app.use(express.json())
-app.use(express.urlencoded({
-  extended: true
-}))
+const express = require("express");
+const chalk = require("chalk");
+const path = require("path");
+const {
+  addNote,
+  getNotes,
+  removeNote,
+  updateNote,
+} = require("./notes.controller");
+const { type } = require("os");
 
-app.get('/', async (req, res) => {
-  res.render('index', {
-    title: 'Express App',
+yargs.command({
+  command: "edit",
+  describe: "edit note",
+  builder: {
+    title: {
+      type: "string",
+      describe: "title note",
+      demandOption: true,
+    },
+    id: {
+      type: "string",
+      describe: "note id",
+      demandOption: true,
+    },
+  },
+  handler({ id, title }) {
+    console.log("edit command", id, title);
+    updateNote({ id, title });
+  },
+});
+yargs.parse();
+
+const port = 3000;
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "pages");
+
+app.use(express.static(path.resolve(__dirname, "public")));
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+
+app.get("/", async (req, res) => {
+  res.render("index", {
+    title: "Express App",
     notes: await getNotes(),
-    created: false
-  })
-})
+    created: false,
+  });
+});
 
-app.post('/', async (req, res) => {
-  await addNote(req.body.title)
-  res.render('index', {
-    title: 'Express App',
+app.post("/", async (req, res) => {
+  await addNote(req.body.title);
+  res.render("index", {
+    title: "Express App",
     notes: await getNotes(),
-    created: true
-  })
-})
+    created: true,
+  });
+});
 
-app.delete('/:id', async (req, res) => {
-  await removeNote(req.params.id)
-  res.render('index', {
-    title: 'Express App',
+app.delete("/:id", async (req, res) => {
+  await removeNote(req.params.id);
+  res.render("index", {
+    title: "Express App",
     notes: await getNotes(),
-    created: false
-  })
-})
+    created: false,
+  });
+});
 
-app.put('/:id', async (req, res) => {
-  await updateNote({ id: req.params.id, title: req.body.title })
-  res.render('index', {
-    title: 'Express App',
+app.put("/:id", async (req, res) => {
+  await updateNote({ id: req.params.id, title: req.body.title });
+  res.render("index", {
+    title: "Express App",
     notes: await getNotes(),
-    created: false
-  })
-})
+    created: false,
+  });
+});
 
 app.listen(port, () => {
-  console.log(chalk.green(`Server has been started on port ${port}...`))
-})
+  console.log(chalk.green(`Server has been started on port ${port}...`));
+});
